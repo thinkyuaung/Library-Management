1adae144cac26972772c5c5b47f5acaff3bd9668
Thin Kyu Aung <thinkyuaung.edka@gmail.com>
2025-11-17 15:14:15 +0630

    Page Transition with React-Transition-Group

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import React, { useRef } from 'react'
+import { Link, Outlet, useLocation } from 'react-router-dom'
 import Nav from '../components/nav'
+import { SwitchTransition,CSSTransition } from 'react-transition-group';
+import './style.css';
+
 
 function Layout() {
+  let location = useLocation();
+  const nodeRef = useRef(null); 
+
   return (
     <div className=''>
         <Nav />
-        <div className='max-w-3xl mx-auto'>
-          <Outlet/>
-        </div>
+
+      <SwitchTransition>
+        <CSSTransition
+          key={location.pathname}
+          classNames='fade'
+          timeout={200}
+          nodeRef={nodeRef}   
+        >
+          <div className='max-w-3xl mx-auto'>
+            <Outlet />
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
+
+        
         
     </div>
   )
